d865615d6f7cbec806fc1e479ef300a8574ec06a
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-19 16:34:14 +0100

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Umair Salam on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioBlitz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BioBlitz
+//
+//  Created by Umair Salam on 4/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
